4c49c3c83e3cff3c5b8aafa9d4f2cf3141a85b6f
Labyrinth759 <valenzuelajoshua759@gmail.com>
2026-01-29 13:19:50 +0800

    landing page (1st code)

--- tableo/src/App.jsx
+++ tableo/src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LandingPage from "./components/LandingPage"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <h1 className='text-9xl text-blue-500'>Carl Pogi</h1>
-      </div>
+      <LandingPage />
     </>
   )
 }
