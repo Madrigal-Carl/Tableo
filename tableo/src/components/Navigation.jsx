c773b6a04b77b854a76bc4ee9db737ece7a51b1a
Labyrinth-759 <valenzuelajoshua759@gmail.com>
2026-01-29 15:27:37 +0800

    Feature/landing page (#13)
    * landing page (1st code)
    
    * changing of logo color

--- tableo/src/components/Navigation.jsx
+++ tableo/src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import logo from '../assets/tb logo.png'
+import logo from '../assets/logo.png'
 
 function Navigation() {
     return (
@@ -6,10 +6,10 @@ function Navigation() {
             <nav className="fixed top-0 left-0 w-full flex justify-between items-center p-4 bg-white shadow-md z-50">
             <img src={logo} alt="Tableo Logo" className="w-30" />
             <div className="flex space-x-6 items-center">
-                <h2 className="text-xl font-bold">
+                <button className="text-xl font-bold">
                 About us
-                </h2>
-                <button className="bg-[#52ab98] text-base text-white px-6 py-3 rounded-lg hover:bg-[#3f8f7f] font-bold">
+                </button>
+                <button className="bg-[#FA824C] text-base text-white px-6 py-3 rounded-3xl hover:bg-[#FF9768] font-bold">
                 Get Started
                 </button>
             </div>
