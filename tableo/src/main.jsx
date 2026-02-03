3e6733c52e168814e55fd50815988c3ed016af73
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-02-03 14:59:40 +0800

    bugfix

--- tableo/src/main.jsx
+++ tableo/src/main.jsx
@@ -4,19 +4,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 //page imports
-import LoginPage from './pages/LoginPage.jsx';
-import RegisterPage from './pages/RegisterPage.jsx';
-import HomePage from './layouts/HomePage.jsx';
+import LoginPage from './layouts/LoginPage.jsx';
+import RegisterPage from './layouts/RegisterPage.jsx';
+import HomePage from './pages/event_admin/HomePage.jsx';
 import LandingPage from './layouts/LandingPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes> 
+      <Routes>
         {/* Routes */}
-        <Route path="/" element={<LandingPage />} />         
-        <Route path="/auth" element={<LoginPage />} />         
-        <Route path="/auth/register" element={<RegisterPage />} /> 
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<LoginPage />} />
+        <Route path="/auth/register" element={<RegisterPage />} />
         <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
