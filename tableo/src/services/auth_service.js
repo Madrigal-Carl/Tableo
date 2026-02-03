b404598d994fe909c19c25baaccb65e510b69511
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-02-03 10:33:32 +0800

    Feature/signup (#52)
    * simple signup with jwt, email code validation not yet completed
    
    * fix signup, added email validation
    
    * Merge branch 'main' into feature/signup
    
    * singup connect to backend

---
+++ tableo/src/services/auth_service.js
@@ -0,0 +1,29 @@
+import api from "./api";
+
+export const signupRequest = (data) => {
+    return api.post("/auth/signup", data);
+};
+
+export const signupVerify = (data) => {
+    return api.post("/auth/signup/verify", data);
+};
+
+export const login = (data) => {
+    return api.post("/auth/login", data);
+};
+
+export const logout = () => {
+    return api.post("/auth/logout");
+};
+
+export const forgotPasswordRequest = (data) => {
+    return api.post("/auth/password/forgot", data);
+};
+
+export const forgotPasswordVerify = (data) => {
+    return api.post("/auth/password/verify", data);
+};
+
+export const forgotPasswordReset = (data) => {
+    return api.post("/auth/password/reset", data);
+};
