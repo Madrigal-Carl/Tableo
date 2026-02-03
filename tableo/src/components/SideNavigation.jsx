a48bef2662ca00a691c80e524632135e71b61e07
Labyrinth-759 <valenzuelajoshua759@gmail.com>
2026-02-03 14:53:07 +0800

    Feature/home page (#56)
    * home page (1st code)
    
    * adding functions
    
    * fixing of home page

---
+++ tableo/src/components/SideNavigation.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import { Calendar, Archive, Settings, LogOut } from "lucide-react";
+
+function SideNavigation() {
+  const [active, setActive] = useState("Events");
+
+    const navItem = (label, Icon) => {
+    const isActive = active === label;
+
+    return (
+        <button
+        onClick={() => setActive(label)}
+        className={`w-full flex items-center gap-3 px-4 py-3 rounded-2xl font-semibold transition
+            ${
+            isActive
+                ? "bg-[#FA824C] text-white hover:bg-[#FF9768]"
+                : "text-gray-700 hover:bg-[#FA824C] hover:text-white"
+            }`}
+        >
+        <Icon size={20} />
+        <span>{label}</span>
+        </button>
+    );
+    };
+
+  return (
+    <aside className="fixed top-0 left-0 h-screen w-72 bg-white shadow-xl z-50 flex flex-col p-6">
+      
+      {/* Logo */}
+      <img
+        src={logo}
+        alt="Tableo Logo"
+        className="w-32 mb-10 self-center"
+      />
+
+      {/* Navigation */}
+      <nav className="flex flex-col gap-2">
+        {navItem("Events", Calendar)}
+        {navItem("Archives", Archive)}
+      </nav>
+
+      {/* Divider */}
+      <div className="border-t border-gray-300 my-6" />
+
+      {/* Settings */}
+      <div className="flex flex-col gap-2">
+        {navItem("Settings", Settings)}
+      </div> 
+
+      {/* Bottom Action */}
+      <div className="mt-auto">
+        {navItem("Logout", LogOut)} 
+      </div>
+    </aside>
+  );
+}
+
+export default SideNavigation;
--- tableo/src/main.jsx
+++ tableo/src/main.jsx
@@ -1,20 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // ✅ import Router
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+
 //page imports
-import LoginPage from './layouts/LoginPage.jsx';
-import RegisterPage from './layouts/RegisterPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
+import HomePage from './layouts/HomePage.jsx';
 import LandingPage from './layouts/LandingPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* ✅ wrap your app with BrowserRouter */}
+    <BrowserRouter>
       <Routes> 
         {/* Routes */}
         <Route path="/" element={<LandingPage />} />         
         <Route path="/auth" element={<LoginPage />} />         
         <Route path="/auth/register" element={<RegisterPage />} /> 
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
