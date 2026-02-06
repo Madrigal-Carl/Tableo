a87301ecaa37d576ce3d42c971b6c345ef50002c
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-02-06 09:56:19 +0800

    backend done

--- backend/src/middlewares/upload.js
+++ backend/src/middlewares/upload.js
@@ -1,21 +1,17 @@
-import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
+const multer = require("multer");
+const cloudinary = require("../config/cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
 
-const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-        folder: "events",
-        allowed_formats: ["jpg", "jpeg", "png", "webp"],
-        transformation: [
-            { width: 1200, height: 630, crop: "limit", quality: "auto" }
-        ],
-    },
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: "events",
+    allowedFormats: ["jpg", "jpeg", "png", "webp"],
+    transformation: [{ width: 1200, height: 630, crop: "limit", quality: "auto" }],
 });
 
 const upload = multer({
-    storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 2MB
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
 });
 
-export default upload;
+module.exports = upload;
