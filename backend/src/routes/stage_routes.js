e9e49a56f7ef59c98a30d8f20bffc21a72718865
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-02-24 15:10:15 +0800

    Feature/advance stage (#178)
    * backend done
    
    * adjustment backend done
    
    * backend done 1.1
    
    * next stage modal populate candidate done

--- backend/src/routes/stage_routes.js
+++ backend/src/routes/stage_routes.js
@@ -1,16 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const requireAuth = require('../middlewares/auth');
+const requireAuth = require("../middlewares/auth");
 const {
-    validateStage,
-    validateStageCount
-} = require('../validators/stage_validator');
-const stageController = require('../controllers/stage_controller');
+  validateStage,
+  validateStageCount,
+  validateStageIdParam,
+} = require("../validators/stage_validator");
+const stageController = require("../controllers/stage_controller");
 
 // Create or update stages by count for an event
-router.post('/event/:eventId', requireAuth, validateStageCount, stageController.createOrUpdateStages);
+router.post(
+  "/event/:eventId",
+  requireAuth,
+  validateStageCount,
+  stageController.createOrUpdateStages,
+);
 
 // Update stage
-router.put('/:id', requireAuth, validateStage, stageController.updateStage);
+router.put("/:id", requireAuth, validateStage, stageController.updateStage);
+
+// Get stage result summary
+router.get(
+  "/:id/results",
+  requireAuth,
+  validateStageIdParam,
+  stageController.getStageResults,
+);
 
 module.exports = router;
