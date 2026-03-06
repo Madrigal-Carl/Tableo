ce1da4e382a0c79b091f430c193c7aae356b5a9a
JoseDoms718 <josedominique018@gmail.com>
2026-03-06 08:20:48 +0800

    Done

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,12 +12,12 @@ require("./scheduler/event_scheduler");
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://192.168.1.3:5173",
     credentials: true,
   },
 });
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://192.168.1.3:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
